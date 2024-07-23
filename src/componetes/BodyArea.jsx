ad95a0b00c5484be3f2b741afb2a09eb943f8feb
Namani31 <kwg531@naver.com>
2024-07-23 15:03:55 +0900

    Refactor: `bodyArea(LeftMenueZone)` 을 HTML -> React

--- src/componetes/BodyArea.jsx
+++ src/componetes/BodyArea.jsx
@@ -1,7 +1,24 @@
+import "../styles/BodyArea.css";
+import { LeftmenuZone } from "./LeftmenuZone";
+import { ContentsZone } from "./ContentsZone";
+
 export function BodyArea(){
   return (
-    <>
-    <p>BodyArea</p>    
-    </>
+    <div id="bodyArea">
+    <LeftmenuZone/>
+    <ContentsZone/>
+    </div>
   );
+
+  /*
+              
+              <!-- 내용Zone시작 -->
+              <style>
+
+              </style>
+              
+              <!-- 내용Zone끝 -->
+          </div>
+          <!-- 본문Area끝 -->
+  */
 }
