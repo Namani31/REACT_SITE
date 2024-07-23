cb82873b670f6942087cc45d9ccea38940523696
Namani31 <kwg531@naver.com>
2024-07-23 17:27:53 +0900

    Refactor: 왼쪽 메뉴 리팩토링

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './styles/App.css';
 import {TopArea} from './componetes/TopArea';
 import {BodyArea} from './componetes/BodyArea';
 import {BottomArea} from './componetes/BottomArea';
+import { useState } from 'react';
 
 function App() {
+  const [leftMenu, setLeftMenu] = useState(4);
+
   return (
     <div className="App">
       <div id="site">
-        <TopArea/>
-        <BodyArea/>
+        <TopArea setLeftMenu={setLeftMenu} />
+        <BodyArea leftMenu = {leftMenu} />
         <BottomArea/>
       </div>
     </div>
