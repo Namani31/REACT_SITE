96c8669e49e92c1b81f670aa42bf1e516eeb5c55
Namani31 <kwg531@naver.com>
2024-07-23 11:21:28 +0900

    Feat: `MenuZone.jsx`에 스타일 적용

--- src/componetes/TitleZone.jsx
+++ src/componetes/TitleZone.jsx
@@ -1,8 +1,23 @@
-export function TitleZone(){
+import '../styles/TitleZone.css';
+import logoImg from '../img/logo.jpg';
+
+export function TitleZone() {
   return (
     <>
-    <p>TitleZone</p>
-    
+      <p>TitleZone</p>
+      <div id="titleZone">
+        <div id="gnb">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Join</a></li>
+            <li><a href="#">Mypage</a></li>
+          </ul>
+        </div>
+        <div id="logo">
+          <img src={logoImg} alt="" />
+        </div>
+      </div>
     </>
   );
 }
